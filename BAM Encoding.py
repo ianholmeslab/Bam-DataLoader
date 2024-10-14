c7b4841df2474ddcd5d39c5012200f8f1f290351
YakunFANG <140862914+YakunFANG@users.noreply.github.com>
2024-10-13 22:07:44 -0700

    Update BAM Encoding.py

--- BAM Encoding.py
+++ BAM Encoding.py
@@ -22,47 +22,56 @@ def extract_sequence_from_fasta(fasta_file, chrom, start, end):
     return None
 
 # Count transitions (A, B, C, D, E, F) from the BAM file, every strand
-def count_transitions(bam_file, chrom, start, end):
-    """ Initialize the count matrix for forward and reverse"""
+def count_transitions(bam_file, chrom, start, end, transition_counts, current_state):
+    # Initializing the transition_variable
     # Opening the bam_file
     with pysam.AlignmentFile(bam_file, "rb") as bam: # Using pysam.AlignmentFile to open a file
-        for pileupcolumn in bam.pileup(chrom, start, end): # Also using the start and end and chrom obtained from the BED file to ectract relevant information from bam file
+        for pileupcolumn in bam.pileup(chrom, start, end): # Confirming the window Also using the start and end and chrom obtained from the BED file to extract relevant information from bam file
             # Every position(base) in the sequence will correspond to one row in the matrix
-            pos = pileupcolumn.pos - start # relevant position with the requested column
-            if 0 <= pos < (end-start):
-                for pileupread in pileupcolumn.pileups: # The pileupread has two versions: the "forward version" & the "reverse version"
-                    """Forward and reverse strand counts"""
-                    if pileupread.alignment.is_reverse:
-                        transition_type = 6 # Starting index for reverse transitions, first original, then reverse version
-                    else:
-                        transition_type = 0 # If the alignment is not in reverse, then the start index would be 0
+            for pileupread in pileupcolumn.pileups: # The pileupread has two versions: the "forward version" & the "reverse version"
+                """Forward and reverse strand counts"""
+                if pileupread.alignment.is_reverse:
+                    strand = 6 # Starting index for reverse transitions, first original, then reverse version
+                else:
+                    strand = 0 # If the alignment is in forward, then the start index would be 0
 
-                    # Count various transitions and update the count matrix accordingly ?????? How to decode the transition conditions??
-                    # And for every version of the strand, there will be a checking to fill its corresponding row in the counting matrix depending the following checking
-                    if pileupread.is_del:
-                        transition_counts[pos][transition_type + 0] += 1
-                    elif pileupread.is_refskip:
-                        transition_counts[pos][transition_type + 1] += 1
-                    elif pileupread.indel > 0:
-                        transition_counts[pos][transition_type + 2] += 1
-                    else:
-                        transition_counts[pos][transition_type + 3] += 1
-                        if pileupread.alignment.query_sequence[pileupread.query_position] != pileupread.reference_base:
-                            transition_counts[pos][transition_type + 4] += 1 # Mismatch
-                        transition_counts[pos][transition_type + 5] += 1
+                if pileupread.cigar:
+                    for cigar in pileupread.cigar:
+                        operation, length = cigar
+                        if current_state == 0 and pileupread.is_head: # A
+                            transition_counts[pileupcolumn.pos][strand + 0] += 1
+                            current_state = 1
+                        elif current_state == 1 and operation != 3: # B
+                            transition_counts[pileupcolumn.pos][strand + 1] += 1
+                            current_state = 1
+                        elif current_state == 1 and operation == 3: # C
+                            transition_counts[pileupcolumn.pos][strand + 2] += 1
+                            current_state = 2
+                        elif current_state == 2 and operation != 3: # D
+                            transition_counts[pileupcolumn.pos][strand + 3] += 1
+                            current_state = 1
+                        elif current_state == 2 and operation == 3: # E
+                            transition_counts[pileupcolumn.pos][strand + 4] += 1
+                            current_state = 2
+                        elif current_state == 1 and pileupread.is_tail: # F
+                            transition_counts[pileupcolumn.pos][strand + 5] += 1
+                            current_state = 3
 
-    return transition_counts
+    return transition_counts, current_state
 
 
 def process_bed_fasta_bam(bed_file, fasta_file, bam_file, output_prefix):
     """Load the BED file using pd.read_csv"""
     bed_df = pd.read_csv(bed_file, sep='\t', header = None, names = ['chromosome', 'start', 'end'])
 
+    current = 0
     for idx, row in bed_df.iterrows():
         chrom = row['chromosome']
         start = row['start']
         end = row['end']
-        length = end - start + 1
+        length = end - start + 1 # the length of current region
+
+        transition_counts = [12][length] # counting matrix for the current region
 
         sequence = extract_sequence_from_fasta(fasta_file, chrom, start, end)
 
@@ -73,10 +82,11 @@ def process_bed_fasta_bam(bed_file, fasta_file, bam_file, output_prefix):
         one_hot_encoded_sequence = one_hot_encoding(sequence)
 
         # Get the 12 counts
-        transition_counts = count_transitions(bam_file, chrom, start, end)
+        transition_matrix, new_current = count_transitions(bam_file, chrom, start, end, transition_counts, current)
+        current = new_current
 
         # Combine the one-hot encoding and the transition counts
-        combined_tensor = np.hstack([one_hot_encoded_sequence, transition_counts])
+        combined_tensor = np.vstack([transition_counts, one_hot_encoded_sequence.T])
 
         # Save the tensor as a numpy file
         output_file = f"{output_prefix}_region_{idx}.npy"
